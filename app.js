b154270574601b852327844f4f8ea6540eb59516
Tara Khanal <tarakh99@gmail.com>
2022-03-14 23:53:07 -0400

    Add UrlEncoded to parse form data

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ mongoose.connect("mongodb://localhost:27017/OldMates");
 
 app.set('view engine', 'ejs');
 app.use(express.static('public')) // To serve static files (css, images, js, etc.)
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use('/', require('./routes/index'));
 
