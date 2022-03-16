4f56536ddc24105fccf2b974f00f8d0cb9f8dcbf
Tara Khanal <tarakh99@gmail.com>
2022-03-15 23:02:31 -0400

    Include session into the project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const User = require('./models/User');
 
 const app = express();
