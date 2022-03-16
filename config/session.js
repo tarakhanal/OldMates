69ebde96c74a9cd9ea8af7dcdee9ecab58e443c6
Tara Khanal <tarakh99@gmail.com>
2022-03-15 22:57:05 -0400

    Add configuration for session

---
+++ config/session.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const sessionConfig = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + (1000 * 60 * 60 * 5), // Expires in 5 hours
+        maxAge: (1000 * 60 * 60 * 5)
+    }
+}
+
+module.exports = sessionConfig;
