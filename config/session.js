4bff5c864d46b54f93d94a0d02913f8aec812327
Tara Khanal <tarakh99@gmail.com>
2022-03-17 11:21:38 -0400

    Add simple authentication

--- config/session.js
+++ config/session.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const sessionConfig = {
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
         httpOnly: true,
         expires: Date.now() + (1000 * 60 * 60 * 5), // Expires in 5 hours
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,7 +5,14 @@ const loggedInUsers = require('./loggedInUsers');
 mongoose.connect("mongodb://localhost:27017/OldMates");
 
 const homeController = (req, res) => {
-    res.render('home', {name: "Tara"});
+    let user = loggedInUsers.get(req.sessionID);
+    
+    if(!user) {
+        console.log("You are not logged in!");
+        res.redirect('/');
+    }
+    else
+        res.render('home', {firstName: user.FirstName, lastName: user.LastName});
 }
 
 module.exports = homeController;
