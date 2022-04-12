1cefa7305e0a4c52b21bce42df26763dc827c836
Tara Khanal <tarakh99@gmail.com>
2022-04-12 13:22:11 -0400

    Make sure user can't login again if they have already logged in

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,3 +1,13 @@
-const defaultController = (req, res) => { res.render('../views/landing')}
+const loggedInUsers = require('./loggedInUsers');
+
+const defaultController = (req, res) => { 
+    let userLoggedIn = loggedInUsers.get(req.sessionID);
+    if(userLoggedIn) {
+        console.log(userLoggedIn.FirstName, "you are already logged in!");
+        res.redirect('/home');
+    }
+    else
+        res.render('../views/landing')
+}
 
 module.exports = defaultController;
